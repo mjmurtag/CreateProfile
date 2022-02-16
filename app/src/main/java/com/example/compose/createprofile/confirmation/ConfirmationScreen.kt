74be19ebdda5c3384bce964e307239c628ba6126
Mike Murtagh <mjmurtag@syr.edu>
2022-02-15 20:40:26 -0800

    Add missing button, screenshot update

--- app/src/main/java/com/example/compose/createprofile/confirmation/ConfirmationScreen.kt
+++ app/src/main/java/com/example/compose/createprofile/confirmation/ConfirmationScreen.kt
@@ -25,15 +25,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -108,6 +106,15 @@ fun Confirm() {
                                 Text(text = user.name, textAlign = TextAlign.Center)
                                 Spacer(modifier = Modifier.height(16.dp))
                                 Text(text = user.email)
+                                Spacer(modifier = Modifier.height(300.dp))
+                                Button(
+                                    onClick = {
+                                    },
+
+                                    modifier = Modifier.fillMaxWidth(),
+                                ) {
+                                    Text(text = stringResource(id = R.string.sign_in))
+                                }
                             }
                         }
                     }
