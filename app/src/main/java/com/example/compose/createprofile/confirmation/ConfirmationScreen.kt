a56d92b89b2f5ccc4bd9931fdf216c87ebbddd25
Mike Murtagh <mjmurtag@syr.edu>
2022-02-15 19:51:18 -0800

    Finalize confirmation screen, add screenshots

--- app/src/main/java/com/example/compose/createprofile/confirmation/ConfirmationScreen.kt
+++ app/src/main/java/com/example/compose/createprofile/confirmation/ConfirmationScreen.kt
@@ -16,9 +16,11 @@
 
 package com.example.compose.createprofile.confirmation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -28,9 +30,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.compose.createprofile.R
 import com.example.compose.createprofile.signup.SignUpViewModel
@@ -69,11 +79,29 @@ fun Confirm() {
                         modifier = Modifier.supportWideScreen()
                     ) {
                         Column {
-                            Text(text = stringResource(id = R.string.hello) + " " + user.name + "!")
+                            Text(text = stringResource(id = R.string.hello) + " " +
+                                    user.name + "!", fontSize = 35.sp)
+                            Spacer(modifier = Modifier.height(16.dp))
                             Text(text = stringResource(id = R.string.confirmation))
-                            Text(text = user.website)
-                            Text(text = user.name)
-                            Text(text = user.email)
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(text = buildAnnotatedString {
+                                    append(
+                                        AnnotatedString(
+                                            text = user.website, spanStyle =
+                                            SpanStyle(Color.Blue)
+                                        )
+                                    )
+                                }, textAlign = TextAlign.Center)
+                                Spacer(modifier = Modifier.height(16.dp))
+                                Text(text = user.name, textAlign = TextAlign.Center)
+                                Spacer(modifier = Modifier.height(16.dp))
+                                Text(text = user.email)
+                            }
                         }
                     }
                 }
